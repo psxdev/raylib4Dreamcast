e40d4f41b282e89de9eb27e202a98b36e5de96dd
psxdev <psxdev@gmail.com>
2024-02-04 18:22:31 +0100

    fix scripts for installation

--- dreamcastbuild.sh
+++ dreamcastbuild.sh
@@ -6,6 +6,6 @@ if test ! $KOS_BASE; then { echo "ERROR: Set \$KOS_BASE installing kos sdk and k
 if test ! $KOS_BASE; then { echo "ERROR: Set \$KOS_PORTS installing kos sdk and kos-ports before continuing."; exit 1; } fi
 
 git clone https://github.com/raylib4Consoles/raylib -b dreamcast
-cd rayib/src
+cd raylib/src
 make 
 make install
