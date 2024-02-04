02797ab41b2f27ab188efeb8232b387dc42e6466
psxdev <psxdev@gmail.com>
2024-02-04 18:23:50 +0100

    Fix KOS_PORTS check

--- dreamcastbuild.sh
+++ dreamcastbuild.sh
@@ -3,7 +3,7 @@
 if test ! $KOS_BASE; then { echo "ERROR: Set \$KOS_BASE installing kos sdk and kos-ports before continuing."; exit 1; } fi
 
 ## Check if KOS_PORTS is set.
-if test ! $KOS_BASE; then { echo "ERROR: Set \$KOS_PORTS installing kos sdk and kos-ports before continuing."; exit 1; } fi
+if test ! $KOS_PORTS; then { echo "ERROR: Set \$KOS_PORTS installing kos sdk and kos-ports before continuing."; exit 1; } fi
 
 git clone https://github.com/raylib4Consoles/raylib -b dreamcast
 cd raylib/src
